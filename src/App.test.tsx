d3561825f237e46563277ad56695ea9a7e11c97c
Myriam <myriam.puchalt@ibm.com>
2021-02-14 15:41:38 +0100

    feat: app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("When the user isn't authenticated", () => {
+  it('"get calls" button should be rendered on page load', () => {
+    render(<App />);
+    const button = screen.queryByText('Get List of Calls');
+    expect(button).toBeInTheDocument();
+  });
+
+  it("pagination buttons should NOT be rendered", () => {
+    render(<App />);
+    const prevButton = screen.queryByText('PREV');
+    const nextButton = screen.queryByText('NEXT');
+    expect(prevButton).not.toBeInTheDocument();
+    expect(nextButton).not.toBeInTheDocument();
+  });
 });
